8dc5d708b766fff668ac42543ecc35f70172088f
AV <arnavagrawalofficial@gmail.com>
2025-04-02 20:59:26 +0530

    Set up Postgres

--- college_backend/college_ranker/settings.py
+++ college_backend/college_ranker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'college_ranker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'collegechayan_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Pass@Raj#tamil143', # Change this
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
