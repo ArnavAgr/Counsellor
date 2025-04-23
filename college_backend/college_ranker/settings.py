49655bee04ffb79655daec534313a10d97b34fa4
AV <arnavagrawalofficial@gmail.com>
2025-04-23 22:58:32 +0530

    minor change

--- college_backend/college_ranker/settings.py
+++ college_backend/college_ranker/settings.py
@@ -140,4 +140,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
      "https://collegechayan.com",  # Your production frontend URL
     "https://www.collegechayan.com",
-    "168.231.119.49"]
+    ]
