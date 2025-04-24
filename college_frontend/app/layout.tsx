deab77f500bf7365ec2c18f751a96c671943b361
AV <arnavagrawalofficial@gmail.com>
2025-04-24 20:28:20 +0530

    Added google analytics file in componenets and added the g-id in layout.tsx

--- college_frontend/app/layout.tsx
+++ college_frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import GoogleAnalytics from './components/GoogleAnalytics';
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GoogleAnalytics />
+        {children}
+        </body>
     </html>
   )
 }
